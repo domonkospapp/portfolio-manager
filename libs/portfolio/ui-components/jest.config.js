de4aa5ca2643774bc2c2131869621a96d99e8877
Domonkos Papp <pappdomonkos96@gmail.com>
2022-02-05 10:26:03 +0100

    Add ui component lib

---
+++ libs/portfolio/ui-components/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'portfolio-ui-components',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/portfolio/ui-components',
+};
